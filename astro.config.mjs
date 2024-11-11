eb0b56d969aa83a28d99d77a1927d576d20b2ade
Xeffen25 <xeffen25@xeffen25.com>
2024-11-11 13:13:17 +0100

    Update SITE config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://cf-pages-astro-starter.pages.dev/",
 	output: "server",
 	adapter: cloudflare({
 		imageService: "compile",
